3c8f2fd0f58a7dd530d83104b0154d7c37175a6d
shakeebur <123761642+shakeebur@users.noreply.github.com>
2023-02-19 09:15:38 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
-import "./styles.css";
+const BASE_URL = 'https://swapi.dev/api/planets/';
+const planetList = [];
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// function to fetch planet data and add to planetList array
+async function fetchPlanets(url) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    data.results.forEach(planet => {
+      const { name, diameter, gravity, climate, population } = planet;
+      planetList.push({ name, diameter, gravity, climate, population });
+    });
+
+    if (data.next) {
+      await fetchPlanets(data.next);
+    } else {
+      const csvData = planetList.map(planet => Object.values(planet).join(','));
+      csvData.unshift('Name,Diameter,Gravity,Climate,Population');
+      const csvContent = csvData.join('\n');
+      const csvBlob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(csvBlob);
+      link.setAttribute('download', 'planets.csv');
+      document.body.appendChild(link);
+      link.click();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// fetch planet data and handle errors using the catch method
+fetch(BASE_URL)
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(`HTTP error: ${res.status}`);
+    }
+    return res.json();
+  })
+  .then(data => fetchPlanets(data.next))
+  .catch(error => console.error(error));
