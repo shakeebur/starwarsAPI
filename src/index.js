89eef549ea6283f6243ec83381a4e7cd8e9b9a3e
Shakeeb Rahman <123761642+shakeebur@users.noreply.github.com>
2023-02-24 03:04:50 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,47 @@
 const BASE_URL = 'https://swapi.dev/api/planets/';
 const planetList = [];
 
-// function to fetch planet data and add to planetList array
 async function fetchPlanets(url) {
   try {
     const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
     const data = await res.json();
 
     data.results.forEach(planet => {
       const { name, diameter, gravity, climate, population } = planet;
-      planetList.push({ name, diameter, gravity, climate, population });
+
+      // Check if any of the relevant data fields are missing or unknown
+      if (diameter !== 'unknown' && gravity !== 'unknown' && climate !== 'unknown' && population !== 'unknown') {
+        planetList.push({ name, diameter, gravity, climate, population });
+      }
     });
 
     if (data.next) {
       await fetchPlanets(data.next);
     } else {
-      const csvData = planetList.map(planet => Object.values(planet).join(','));
-      csvData.unshift('Name,Diameter,Gravity,Climate,Population');
-      const csvContent = csvData.join('\n');
-      const csvBlob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-      const link = document.createElement('a');
-      link.href = URL.createObjectURL(csvBlob);
-      link.setAttribute('download', 'planets.csv');
-      document.body.appendChild(link);
-      link.click();
+      const csv = convertToCSV(planetList);
+      downloadCSV(csv);
     }
   } catch (error) {
-    console.error(error);
+    console.error('Error fetching planet data:', error);
   }
 }
 
-// fetch planet data and handle errors using the catch method
-fetch(BASE_URL)
-  .then(res => {
-    if (!res.ok) {
-      throw new Error(`HTTP error: ${res.status}`);
-    }
-    return res.json();
-  })
-  .then(data => fetchPlanets(data.next))
-  .catch(error => console.error(error));
+function convertToCSV(arr) {
+  const header = Object.keys(arr[0]).join(',');
+  const rows = arr.map(obj => Object.values(obj).join(','));
+  return [header, ...rows].join('\n');
+}
+
+function downloadCSV(csv) {
+  const blob = new Blob([csv], { type: 'text/csv' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.setAttribute('href', url);
+  a.setAttribute('download', 'planet-data.csv');
+  a.click();
+}
+
+fetchPlanets(BASE_URL);
